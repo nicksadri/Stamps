d64ca291fe6bd7e06aaafeda98d9c42a07184fb3
nicksadri <sadri.nick@gmail.com>
2025-03-27 01:12:21 +0100

    Initial Commit

---
+++ Stamps/StampsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampsApp.swift
+//  Stamps
+//
+//  Created by Nick Sadri on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct StampsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StampsTests/StampsTests.swift
@@ -0,0 +1,17 @@
+//
+//  StampsTests.swift
+//  StampsTests
+//
+//  Created by Nick Sadri on 3/27/25.
+//
+
+import Testing
+@testable import Stamps
+
+struct StampsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
